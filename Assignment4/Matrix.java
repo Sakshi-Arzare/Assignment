8dae002beadf9a4314b9f59995bc7018017f6a44
Sakshi_Arzare_ <100848092+Sakshi-Arzare@users.noreply.github.com>
2022-04-02 00:29:30 +0530

    Add files via upload

---
+++ Assignment4/Matrix.java
@@ -0,0 +1,22 @@
+//write a program for matrix.
+import java.util.*;
+class Matrix{
+public static void main(String args[]){
+	int a[][] = {{3,6,9},{12,15,18},{21,24,27}};
+    
+    for(int i=0; i<a.length; i++){
+      for(int j=0;j<a.length;j++){
+          System.out.print(a[i][j] + " ");
+          }
+		  System.out.println();
+       }
+	   
+	    
+}
+}
+
+
+
+		
+
+	
